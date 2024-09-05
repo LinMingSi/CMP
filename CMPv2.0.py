26a3f4337b208fe81648d26ed5f9fdd87fa84390
771835 <150520586+771835@users.noreply.github.com>
2024-09-05 22:52:53 +0800

    更新cmp
    a优化了算法，但会删掉.minecraft下的所有screenshots文件夹中的.png,

--- CMPv2.0.py
+++ CMPv2.0.py
@@ -10,25 +10,18 @@ def clean_folder():
         tk.messagebox.showinfo(title='提示', message='未选择.minecraft文件夹路径')
         return
     remove_file=[]
-    screenshots_path = os.path.join(path, 'screenshots')#合成非版本文件夹内screenshots路经
-    if os.path.exists(screenshots_path):#判断screenshots文件夹是否存在
-        for filename in os.listdir(screenshots_path):#遍历screenshots文件夹
-            if filename.endswith('.png'):#判断文件末尾是否为png
-                num+=1#记录清理截图数
-                size+=os.path.getsize(os.path.join(screenshots_path, filename))#记录被清理截图大小
-                remove_file.append(os.path.join(screenshots_path, filename))#记录被清理截图路经
-    versions_path = os.path.join(path, 'versions')#合成版本文件夹内screenshots路经
-    if os.path.exists(versions_path):#判断版本文件夹是否存在
-        for version_folder in os.listdir(versions_path):#遍历版本文件夹
-            version_path = os.path.join(versions_path, version_folder)
-            if os.path.isdir(version_path):
-                version_screenshots_path = os.path.join(version_path, 'screenshots')
-                if os.path.exists(version_screenshots_path):#判断screenshots文件夹是否存在
-                    for filename in os.listdir(version_screenshots_path):#遍历screenshots文件夹
-                        if filename.endswith('.png'):#判断文件末尾是否为png
-                            num+=1#记录清理截图数
-                            size+=os.path.getsize(os.path.join(version_screenshots_path, filename))#记录被清理截图大小
-                            remove_file.append(os.path.join(version_screenshots_path, filename))
+    screenshots_dirs=set()#创建一个集合，以免一个文件夹被多次记录
+    for dirpath, dirnames, _ in os.walk(path):
+        if 'screenshots'==os.path.basename(dirpath):
+            screenshots_dirs.add(dirpath)
+            print(f"发现screenshots文件夹:{dirpath}")
+    for screenshots_dir in screenshots_dirs:
+        if os.path.exists(screenshots_dir):#判断screenshots文件夹是否存在
+            for filename in os.listdir(screenshots_dir):#遍历screenshots文件夹
+                if filename.endswith('.png'):#判断文件末尾是否为png
+                    num+=1#记录清理截图数
+                    size+=os.path.getsize(os.path.join(screenshots_dir, filename))#记录被清理截图大小
+                    remove_file.append(os.path.join(screenshots_dir, filename))#记录被清理截图路经
     for i in remove_file:
         print(f"remove:{i}")
         os.remove(i)
@@ -39,7 +32,7 @@ def show_warning():
     warning = tk.Toplevel()
     warning.title('警告')
     warning.geometry('700x300')
-    warning_label = tk.Label(warning, text='注意:这将清理游戏截图文件夹下所有后缀名为.png的文件（应该没人会把重要的图片放在这里吧）\n声明:作者不会承担任何因为CMP而造成的图片丢失的责任', font=('微软雅黑', 12))
+    warning_label = tk.Label(warning, text='注意:这将清理游戏截图文件夹(名为screenshots的目录)下所有后缀名为.png的文件（应该没人会把重要的图片放在这里吧）\n声明:作者不会承担任何因为CMP而造成的图片丢失的责任', font=('微软雅黑', 12))
     warning_label.pack(pady=10)
     confirm_button = tk.Button(warning, text='我已知晓，重要图片已转移，启动清理！', font=('微软雅黑', 12), command=lambda : [warning.destroy(), clean_folder()])
     confirm_button.pack(pady=20)
@@ -49,12 +42,12 @@ def show_warning():
 # 创建主窗口
 root = tk.Tk()
 root.title('欢迎使用CMPv2.0')
-root.geometry('400x560')  # 调整窗口大小
+root.geometry('400x300')  # 调整窗口大小
 # 设置窗口图标
 if os.path.exists('icon.ico') :#判断图标文件是否存在
     root.iconbitmap('icon.ico')  
 # 添加标题
-title = tk.Label(root, text='欢迎使用CMPv1.0', font=('宋体', 20), bd=2, relief='solid', width=20, fg='green')
+title = tk.Label(root, text='欢迎使用CMPv2.0', font=('宋体', 20), bd=2, relief='solid', width=20, fg='green')
 title.pack(pady=10)
 
 # 添加作者信息
